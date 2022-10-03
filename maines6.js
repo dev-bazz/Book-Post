73b4bda58340c3bfc77338bfe1a0da2536b693e5
bazz <clement.f.bazz@gmail.com>
2022-10-03 21:36:11 +0100

    es6 OOP

---
+++ maines6.js
@@ -0,0 +1,10 @@
+"use strict";
+// Book Contructor
+class e_book {
+    // ------------------------------------------------------------------------------------------------
+    constructor(title, author, ismb) {
+        this.title = title;
+        this.author = author;
+        this.ismb = ismb;
+    }
+}
---
+++ maines6.ts
@@ -0,0 +1,13 @@
+// Book Contructor
+class e_book {
+        title: string;
+        author:string;
+        ismb:number;
+// ------------------------------------------------------------------------------------------------
+        constructor(title:string, author:string, ismb:number){
+                this.title = title;
+                this.author = author;
+                this.ismb = ismb;
+        }
+//-----------------------------------------------------------------------------------
+}
